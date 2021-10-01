f89f37a7c9990fac1c34312ec71cc9605843c237
cschuhmacher <chris.r.schuhmacher@gmail.com>
2021-10-01 15:38:25 -0700

    Add express server and clear node_modules

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.get('/products', (req, res) => {
+  res.send('get all products');
+});
+
+app.get('/products/:product_id', (req, res) => {
+  res.send(`get product by product id : ${req.params.product_id}`);
+});
+
+app.get('/products/:product_id/styles', (req, res) => {
+  res.send(`get styles by product id : ${req.params.product_id}`);
+});
+
+app.get('/products/:product_id/related', (req, res) => {
+  res.send(`get related products ids by product id : ${req.params.product_id}`);
+});
+
+app.get('/cart', (req, res) => {
+  res.send('get all products in cart');
+});
+
+app.post('/cart', (req, res) => {
+  res.send('post an item to the cart');
+});
+
+app.listen(port, () => {
+  console.log(`Server is now listening on Port ${port}`);
+});
