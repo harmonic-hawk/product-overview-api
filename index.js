7c15f2ced686dee2e951728c97d1638a37827921
cschuhmacher <chris.r.schuhmacher@gmail.com>
2021-10-04 18:25:44 -0700

    Change port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const db = require('./db/mydb');
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
